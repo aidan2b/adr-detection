6578edb494f863bfe4346165fa030054585895f5
aidan2b <aidanb@naver.com>
2023-04-19 22:57:17 -0400

    fixed indentation issues

--- medicrawl.sh
+++ medicrawl.sh
@@ -6,64 +6,60 @@ for medication in $(kubectl get configmap $CONFIGMAP_NAME -o jsonpath="{.data.$K
 do
   cat <<EOF | kubectl apply -f -
 apiVersion: v1
-kind: pod
+kind: Pod
 metadata:
   name: medicrawl-pod-$medication
 spec:
-  template:
-restartPolicy: Never
-containers:
-- name: medicrawl
-    image: aidan2b/adr-detection:test
-    command: ["/bin/sh", "-c"]
-    env:
-    - name: MEDICATION_NAME
-        value: "$medication"
-    - name: REDDIT_CLIENT_ID
-        valueFrom:
-        secretKeyRef:
-            name: adr-detection-secrets
-            key: REDDIT_CLIENT_ID
-    - name: REDDIT_CLIENT_SECRET
-        valueFrom:
-        secretKeyRef:
-            name: adr-detection-secrets
-            key: REDDIT_CLIENT_SECRET
+  restartPolicy: Never
+  containers:
+    - name: medicrawl
+      image: aidan2b/adr-detection:test
+      command: ["/bin/sh", "-c"]
+      env:
+        - name: MEDICATION_NAME
+          value: "$medication"
+        - name: REDDIT_CLIENT_ID
+          valueFrom:
+            secretKeyRef:
+              name: adr-detection-secrets
+              key: REDDIT_CLIENT_ID
+        - name: REDDIT_CLIENT_SECRET
+          valueFrom:
+            secretKeyRef:
+              name: adr-detection-secrets
+              key: REDDIT_CLIENT_SECRET
+      args:
+        - |
+          # Run the pipeline script
+          python run_pipeline.py
 
-    args:
-    - |
-        # Run the pipeline script
-        python run_pipeline.py
-
-        cp -r /shiny_app/faers.csv /data/faers.csv
-        tail -n +2 /shiny_app/linked_data.csv >> /data/linked_data.csv
-
-    resources:
-    limits:
-        memory: 12Gi
-        cpu: 2
-        nvidia.com/gpu: 1
-    requests:
-        memory: 12Gi
-        cpu: 2
-        nvidia.com/gpu: 1
-    volumeMounts:
-    - mountPath: /data
-        name: adr-detection-pvc
-volumes:
-- name: adr-detection-pvc
-    persistentVolumeClaim:
-    claimName: adr-detection-pvc
-affinity:
-nodeAffinity:
-    requiredDuringSchedulingIgnoredDuringExecution:
-    nodeSelectorTerms:
-        - matchExpressions:
-            - key: nvidia.com/gpu.product
-            operator: In
-            values:
-                - NVIDIA-GeForce-RTX-3090
-                - Tesla-T4
-
+          cp -r /shiny_app/faers.csv /data/faers.csv
+          tail -n +2 /shiny_app/linked_data.csv >> /data/linked_data.csv
+      resources:
+        limits:
+          memory: 12Gi
+          cpu: 2
+          nvidia.com/gpu: 1
+        requests:
+          memory: 12Gi
+          cpu: 2
+          nvidia.com/gpu: 1
+      volumeMounts:
+        - mountPath: /data
+          name: adr-detection-pvc
+  volumes:
+    - name: adr-detection-pvc
+      persistentVolumeClaim:
+        claimName: adr-detection-pvc
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+          - matchExpressions:
+              - key: nvidia.com/gpu.product
+                operator: In
+                values:
+                  - NVIDIA-GeForce-RTX-3090
+                  - Tesla-T4
 EOF
 done
